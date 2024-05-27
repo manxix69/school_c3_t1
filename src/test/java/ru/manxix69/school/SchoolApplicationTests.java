592a763ed6cfe7ee71f1c2bd807e05d4e0499e6d
manxix69 <manxix69@gmail.com>
2024-05-27 22:30:47 +0500

    Initial commit

---
+++ src/test/java/ru/manxix69/school/SchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.manxix69.school;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
