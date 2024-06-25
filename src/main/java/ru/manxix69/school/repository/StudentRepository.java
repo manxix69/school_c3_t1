046295b54f64b7b7cfffa6470e9a2caee211ea7f
manxix69 <manxix69@gmail.com>
2024-06-26 00:27:40 +0500

    created sql query and end-points

--- src/main/java/ru/manxix69/school/repository/StudentRepository.java
+++ src/main/java/ru/manxix69/school/repository/StudentRepository.java
@@ -1,6 +1,7 @@
 package ru.manxix69.school.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import ru.manxix69.school.model.Student;
 
 import java.util.Collection;
@@ -11,4 +12,13 @@ public interface StudentRepository extends JpaRepository<Student, Long> {
 
     Collection<Student> findByAgeBetween(int minAge, int maxAge);
 
+    @Query(value = "SELECT COUNT(1) as count FROM student", nativeQuery = true)
+    Integer getCountStudents();
+
+    @Query(value = "SELECT AVG(age) as average FROM student", nativeQuery = true)
+    Integer getAverageAgeStudents();
+
+    //получаем последние 5 студентов по id
+    @Query(value = "select s.* from student as s order by s.id desc limit 5", nativeQuery = true)
+    Collection<Student> getLastStudents();
 }
