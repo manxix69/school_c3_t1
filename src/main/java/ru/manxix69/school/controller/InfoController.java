f2529e8372fa4ee561d9f6b9f1d4fbd954e18537
manxix69 <manxix69@gmail.com>
2024-07-07 15:01:11 +0500

    create end poin info

---
+++ src/main/java/ru/manxix69/school/controller/InfoController.java
@@ -0,0 +1,26 @@
+package ru.manxix69.school.controller;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.manxix69.school.service.InfoService;
+import ru.manxix69.school.service.InfoServiceImp;
+
+@Controller
+@RequestMapping("info")
+public class InfoController {
+
+    private final InfoService infoService;
+
+    public InfoController(InfoService infoService) {
+        this.infoService = infoService;
+    }
+
+    @GetMapping("port")
+    public ResponseEntity<Integer> getPort() {
+        Integer port = infoService.getPort();
+        return ResponseEntity.ok(port);
+    }
+}
